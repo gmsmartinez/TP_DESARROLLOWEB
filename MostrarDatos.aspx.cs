5582248b01c7af5aee0326eb254f31e73db0b727
Gabriela <55298017+gmsmartinez@users.noreply.github.com>
2024-06-22 23:25:17 -0300

    DB y Validaciones agregadas

---
+++ MostrarDatos.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class MostrarDatos : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            lblapellidoIngresado.InnerText = Page.Session["apellido"].ToString();
+            lblnombreIngresado.InnerText = Page.Session["nombre"].ToString();
+            lbldniIngresado.InnerText = Page.Session["dni"].ToString();
+            lblemailIngresado.InnerText = Page.Session["email"].ToString();
+            lblTurnoIngresado.InnerText = Page.Session["turno"].ToString();
+
+        }
+
+        protected void Guardar_Click(object sender, EventArgs e)
+        {
+            ConexionDBDataContext instancia = new ConexionDBDataContext();
+            Comentario coment = new Comentario();
+            coment.DNI = Convert.ToInt32(Page.Session["dni"].ToString());
+            coment.Apellido = Page.Session["apellido"].ToString();
+            coment.Comentario1 = txtcomentario.Text;
+
+            instancia.Comentarios.InsertOnSubmit(coment);
+            instancia.SubmitChanges();
+
+            Response.Redirect("IngresoAlumno.aspx");
+        }
+    }
+}
